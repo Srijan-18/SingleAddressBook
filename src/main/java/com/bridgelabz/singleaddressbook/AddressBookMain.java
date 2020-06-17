5ca4c4a7e9fca98972a2b6a661a5e13397a93424
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-17 20:28:31 +0530

    Display of Welcome Message at console

---
+++ src/main/java/com/bridgelabz/singleaddressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.singleaddressbook;
+
+public class AddressBookMain {
+    public static void main (String args[]){
+        System.out.print("\t\t\t\t*****WELECOME TO ADDRESSBOOK PROGRAM*****\n\n");
+    }
+}
